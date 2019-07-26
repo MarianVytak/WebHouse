ff7403cab628be8af4088c4c94999c54d4f6ba4a
MarianVytak <marianvytak@gmail.com>
2019-07-26 23:32:55 +0300

    Start work

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        main: "./src/js/index.js",
+    },
+
+    output: {
+        filename: "[name].js",
+        chunkFilename: "[name].js",
+        publicPath: "/"
+    },
+
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /node_modules/,
+                    chunks: "initial",
+                    name: "vendor",
+                    enforce: true
+                }
+            }
+        }
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: require.resolve("babel-loader"),
+                    query: {
+                        presets: [
+                            ["@babel/preset-env", { modules: false }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+            },
+            {
+                test: /\.css$/i,
+                use: ['to-string-loader', 'css-loader'],
+            },
+        ]
+    },
+
+    resolve: {
+        alias: {
+            "%modules%": path.resolve(__dirname, "src/blocks/modules")
+        }
+    }
+};
